dee653c8edcdc42dfab8a7b807f6e7a38c5219e0
YASHRAJ MAURYA <yashrajmaurya1234@gmail.com>
2024-10-05 21:34:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 submitBtn.addEventListener('click', async function (e) {
-    resultCont.innerHTML = `<img width="100px" src="img/loading.svg" alt="loading">`
-    e.preventDefault()
-    let key = 'ema_live_gJuPIFjTd4ikAbYH4RGDygDZMZZ5RTLwju4jWHmE'
     let email = document.getElementById('email').value
-    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
-    let res = await fetch(url)
-    let result = await res.json()
-    let str = ''
-    for (key of Object.keys(result)) {
-        if (result[key] !== "" && result[key] !== " ") {
-            str = str + `<div>${key}: ${result[key]}</div>`
+    if (email !== "" && email !== " ") {
+        resultCont.innerHTML = `<img width="100px" src="img/loading.svg" alt="loading">`
+        e.preventDefault()
+        let key = 'ema_live_gJuPIFjTd4ikAbYH4RGDygDZMZZ5RTLwju4jWHmE'
+        let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+        let res = await fetch(url)
+        let result = await res.json()
+        let str = ''
+        for (key of Object.keys(result)) {
+            if (result[key] !== "" && result[key] !== " ") {
+                str = str + `<div>${key}: ${result[key]}</div>`
+            }
         }
+        resultCont.innerHTML = str
+    } else {
+        alert('Please enter your email')
     }
-    resultCont.innerHTML = str
 })
